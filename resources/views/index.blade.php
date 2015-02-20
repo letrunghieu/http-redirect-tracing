9bef14e6fc78e854f5c7b1253bd1769be815a944
Hieu Le <letrunghieu.cse09@gmail.com>
2015-02-20 05:40:23 +0000

    implement index interface

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.master')
 
 @section('content')
-Foo
+<h1 id="page-title" class="text-center">
+@lang('app.http redirect tracing')
+<h1>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2 text-center">
+        {!! Form::open() !!}
+        <div class="form-group form-group-lg">
+        {!! Form::text('url', null, ['class' => 'form-control input-lg', 'placeholder' => trans('form.enter your url here')]) !!}
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary"> 
+                <i class="glyphicon glyphicon-ok"></i>
+                <span>
+                @lang('form.submit')
+                </span>
+            </button>
+        </div>
+        {!! Form::close() !!}
+    </div>
+</div>
 @stop
