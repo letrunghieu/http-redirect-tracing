7c032c7e577eac4d275a30ef7a044335927b1556
Hieu Le <letrunghieu.cse09@gmail.com>
2015-03-20 17:22:04 +0700

    Add nofollow

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -6,7 +6,7 @@ return [
     'http redirect tracing'                                                => 'Trace your HTTP redirects',
     'enter an URL and see details how many times the browser redirect you' => 'Enter an URL and see details how many times the browser redirect you',
     'tracing result of :url'                                               => 'Tracing result of: <a href=":url">:url</a>',
-    ':effective is the effective url of'                                   => '<a href=":effective">:effective</a> is the effective URL of',
+    ':effective is the effective url of'                                   => '<a href=":effective" rel="nofollow">:effective</a> is the effective URL of',
     'source code is released under AGPL license'                           => 'Source code is released under <a href="https://github.com/letrunghieu/http-redirect-tracing/blob/master/LICENSE">AGPL</a> license',
     'home page'                                                            => 'Home page',
     'my cv'                                                                => 'My CV',
--- resources/lang/vi/app.php
+++ resources/lang/vi/app.php
@@ -1,12 +1,16 @@
 <?php
 
 return [
-	'name' => 'HTTP redirect tracer',
-	'about this app' => 'Giới thiệu',
-	'http redirect tracing' => 'Theo dõi chuyển hướng HTTP',
-	'enter an URL and see details how many times the browser redirect you' => 'Nhập một URL và xem trình duyệt chuyển hướng bạn như thế nào
-',
-    'tracing result of :url' => 'Tracing result of: <a href=":url">:url</a>',
-    ':effective is the effective url of' => '<a href=":effective">:effective</a> là URL hiệc lực của',
-    'source code is released under AGPL license' => 'Mã nguồn được phát hành dưới giấy phép <a href="https://github.com/letrunghieu/http-redirect-tracing/blob/master/LICENSE">AGPL</a>',
+    'name'                                                                 => 'HTTP redirect tracer',
+    'about this app'                                                       => 'Giới thiệu',
+    'http redirect tracing'                                                => 'Theo dõi chuyển hướng HTTP',
+    'enter an URL and see details how many times the browser redirect you' => 'Nhập một URL và xem trình duyệt chuyển hướng bạn như thế nào',
+    'tracing result of :url'                                               => 'Tracing result of: <a href=":url">:url</a>',
+    ':effective is the effective url of'                                   => '<a href=":effective" rel="nofollow">:effective</a> là URL hiệc lực của',
+    'source code is released under AGPL license'                           => 'Mã nguồn được phát hành dưới giấy phép <a href="https://github.com/letrunghieu/http-redirect-tracing/blob/master/LICENSE">AGPL</a>',
+    'home page'                                                            => 'trang chủ',
+    'my cv'                                                                => 'CV của tôi',
+    'errors'                                                               => [
+        'page not found' => 'Trang này không tồn tại!'
+    ],
 ];
